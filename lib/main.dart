73ea12b2bbfbbc0d016d9841aee73d0f992b72dd
Devin Jean <emailcruzjean@yahoo.com>
2023-06-21 15:03:34 -0500

    menu

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'ffi.dart' if (dart.library.html) 'ffi_web.dart';
 import 'dart:async';
 
+const updateInterval = Duration(milliseconds: 500);
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,48 +14,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PhoneIoT',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'PhoneIoT'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
+  final TextEditingController serverAddr = TextEditingController();
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late String deviceID;
+  late String devicePW;
+
   late Status status;
   late Timer? timer;
+
+  bool menuOpen = true;
 
   @override
   void initState() {
     super.initState();
+
+    deviceID = 'test id';
+    devicePW = 'test pass';
+
     api.initialize();
     status = const Status(errors: []);
 
     void update() {
-      api.getStatus().then((res) => setState(() {
-        status = res;
-        timer = Timer(const Duration(milliseconds: 500), update);
-      }));
+      api.getStatus()
+        .then((res) {
+          setState(() => status = res);
+          timer = Timer(updateInterval, update);
+        })
+        .catchError((e) {
+          debugPrint('update exception: $e');
+          timer = Timer(updateInterval, update);
+        });
     }
     update();
   }
@@ -68,19 +76,104 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     final theme = Theme.of(context).textTheme;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    final menu = Container(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black45,
+            blurRadius: 10,
+          ),
+        ],
       ),
-      body: Center(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("NetsBlox VM", style: theme.headlineMedium),
-            Text('errors: ${status.errors}', style: theme.headlineSmall),
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Image.asset('assets/images/AppIcon-512-trans.png', height: 60, isAntiAlias: true),
+            Text(widget.title, style: theme.headlineSmall),
+            const SizedBox(height: 10),
+            Text('Device ID: $deviceID'),
+            Text('Password: $devicePW'),
+            const SizedBox(height: 10),
+            SizedBox(
+              width: 250,
+              child: TextFormField(
+                controller: widget.serverAddr,
+                textAlign: TextAlign.center,
+                decoration: const InputDecoration(
+                  floatingLabelAlignment: FloatingLabelAlignment.center,
+                  alignLabelWithHint: true,
+                  labelText: 'Server Address',
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _connect,
+              child: const Text('Connect'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: _reconnect,
+              child: const Text('Reconnect'),
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: _newPassword,
+              child: const Text('New Password'),
+            ),
           ],
         ),
       ),
     );
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: InkWell(
+          onTap: () => setState(() => menuOpen ^= true),
+          child: const Icon(
+            Icons.list,
+          )
+        ),
+        title: Text(widget.title),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.white,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("NetsBlox VM", style: theme.headlineMedium),
+                  Text('errors: ${status.errors}', style: theme.headlineSmall),
+                ],
+              ),
+            ),
+          ),
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 10000),
+            left: menuOpen ? 20 : -300,
+            top: 20,
+            curve: Curves.ease,
+            child: menu,
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _connect() {
+    print('connect');
+  }
+  void _reconnect() {
+    print('reconnect');
+  }
+
+  void _newPassword() {
+    print('new password');
   }
 }
