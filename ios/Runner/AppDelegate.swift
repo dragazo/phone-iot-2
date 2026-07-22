d469362e2ee954e2ecd8b1495198699a824a23ad
Devin Jean <emailcruzjean@yahoo.com>
2026-07-22 08:35:25 -0500

    more ios updates

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,13 +2,15 @@ import UIKit
 import Flutter
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     print("dummy_value=\(dummy_method_to_enforce_bundling())");
-    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
